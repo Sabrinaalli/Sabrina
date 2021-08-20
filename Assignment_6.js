4a32bd9162f5c074d6758b3ae5a888563815b61f
Sabrinaalli <89237321+Sabrinaalli@users.noreply.github.com>
2021-08-19 23:32:08 -0500

    Prework files

---
+++ Assignment_6.js
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2513
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 var array = [ Retta, Vern, Marica, Cyrus, Linette, Curt, Nichole, Jesica, Wynell, Michaele ]\
+var array_length = console.log(array.length)\
+\
+for(var i=0; i<array.length; i++)\{\
+   var count = array[i].length;\
+   if (count < count)\{\
+     console.log(\'93The mayor length is: \'94 + count)\
+\};\
+else\{\
+\};\
+   \
+\};}
---
+++ assignment_4.js
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2513
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 var over_under_array = [1,45,32,21,5,17,43,93]\
+console.log(over_under_array.length)\
+\
+for(var i=0; i<over_under_array.length; i++)\{\
+      console.log(i)\
+if (i > 25)\{\
+    print \'93over\'94\
+\};\
+else\{\
+    print \'93under\'94\
+\};\
+\};}
